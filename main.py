942c08d9d0bdb360bbcd918bcb1249fa00949ff1
Evgeny M. Mitushin <mitushinem@gmail.com>
2024-08-31 14:25:34 +0300

    Домашнее задание по теме "Хендлеры обработки сообщений".

---
+++ main.py
@@ -0,0 +1,27 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+from config import API_KEY
+
+
+bot = Bot(token=API_KEY)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(text=['Urban', 'ff'])
+async def Urban(message: types.Message):
+    print(message.text)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def start(message: types.Message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
